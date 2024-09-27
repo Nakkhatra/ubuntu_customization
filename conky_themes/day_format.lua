8f89e0639512ac92641b8ff1a7e9f324d145a3b5
Nakkhatra <msnakkhatra@gmail.com>
2024-09-27 16:51:08 +0600

    Updated date text position for smaller string dates like Friday, Sunday etc, if string.len<=6

--- conky_themes/day_format.lua
+++ conky_themes/day_format.lua
@@ -5,8 +5,14 @@ function conky_format_day()
     -- Capitalize the first letter, lowercase the rest
     local formatted_day = string.sub(day, 1, 1):upper() .. string.sub(day, 2):lower()
 
-    -- Return the formatted day
-    return formatted_day
+    -- Add spaces for shorter day names to adjust the position
+    if string.len(day) <= 6 then
+        offset_spaces = " "  -- Add some spaces for adjustment
+        return offset_spaces .. formatted_day
+    else
+        -- Return the formatted day without additional spaces
+        return formatted_day
+    end
 end
 
 
